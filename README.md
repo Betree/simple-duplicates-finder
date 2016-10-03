8f829d0e080e0751900d550c659048b7cfc3f0d6
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2016-10-03 16:39:00 +1100

    add forgotten modifications

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MelissaListDuplicates
+# Simple duplicates finder
 
 This project was generated with [angular-cli](https://github.com/angular/angular-cli) version 1.0.0-beta.16.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  firstListStr: string = '';
+  secondListStr: string = '';
+  uniqueFirstList: string = '';
+  uniqueSecondList: string = '';
+  hasChanges: boolean = false;
+
+  refresh_uniques(): void {
+    let firstList: string[] = this.firstListStr.split('\n').map(s => s.toLocaleLowerCase());
+    let secondList: string[] = this.secondListStr.split('\n').map(s => s.toLocaleLowerCase());
+
+    this.uniqueFirstList = firstList.filter(e => secondList.indexOf(e) == -1).join('\n');
+    this.uniqueSecondList = secondList.filter(e => firstList.indexOf(e) == -1).join('\n');
+
+    this.hasChanges = false;
+  }
+
+  is_result_disabled(): boolean {
+    return this.hasChanges || (!this.firstListStr.length && ! this.secondListStr.length);
+  }
 }
